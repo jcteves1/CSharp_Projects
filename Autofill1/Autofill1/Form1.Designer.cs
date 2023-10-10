95ed73aee257c631cf9d84058d2dddc28e24f94d
jcteves1 <45355793+jcteves1@users.noreply.github.com>
2023-10-11 07:38:59 +0800

    Add files via upload

---
+++ Autofill1/Autofill1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Autofill1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbTest = new System.Windows.Forms.TextBox();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbTest
+            // 
+            this.tbTest.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.None;
+            this.tbTest.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.tbTest.Location = new System.Drawing.Point(45, 29);
+            this.tbTest.Name = "tbTest";
+            this.tbTest.Size = new System.Drawing.Size(511, 22);
+            this.tbTest.TabIndex = 0;
+            this.tbTest.TextChanged += new System.EventHandler(this.tbTest_TextChanged);
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(45, 79);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(511, 321);
+            this.tbResult.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 450);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.tbTest);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbTest;
+        private System.Windows.Forms.TextBox tbResult;
+    }
+}
+
---
+++ Autofill1/Autofill1/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Autofill1
+{
+    public partial class Form1 : Form
+    {
+        //SqlConnection con = new SqlConnection("Data Source=LAPTOP-5VQ1AK2J\\SQLEXPRESS;Initial Catalog=Dictionary;Integrated Security=True");
+        //SqlCommand cmd;
+        //SqlDataReader rdr;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbTest_TextChanged(object sender, EventArgs e)
+        {
+            if (tbTest.Text == "")
+            {
+                tbResult.Clear();
+                return;
+            }
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-5VQ1AK2J\\SQLEXPRESS;Initial Catalog=Dictionary;Integrated Security=True");
+            con.Open();
+
+            //if (rdr != null && !rdr.IsClosed)
+            //{
+            //    rdr.Close();
+            //}
+            tbResult.Text = "";
+            string searchText = tbTest.Text,
+                   word,
+                   definition,
+                   date;
+            string query =
+                "SELECT TOP 5 word, definition, date " +
+                "FROM dictionary " +
+                "WHERE word LIKE @Word + '%' ";
+            try
+            {
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Word", searchText);
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    word = rdr.GetString(0);
+                    definition = rdr.GetString(1);
+                    date = rdr.GetString(2);
+                    tbResult.Text += $"{word}         {date}\r\n{definition}\r\n";
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(
+                    "An error occurred while querying the database: " + ex.Message,
+                    "Error", 
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Error);
+            }
+              
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-5VQ1AK2J\\SQLEXPRESS;Initial Catalog=Dictionary;Integrated Security=True");
+            con.Open();
+            string query =
+                "SELECT word " +
+                "FROM dictionary";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while(rdr.Read())
+            {
+                string suggestion = rdr.GetString(0);
+                tbTest.AutoCompleteCustomSource.Add(suggestion);
+            }
+            rdr.Close();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            
+        }
+    }
+}
