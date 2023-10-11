87cd96dced6236b7ff6e02aff484363a1d924fad
jcteves1 <45355793+jcteves1@users.noreply.github.com>
2023-10-11 18:16:26 +0800

    new version

---
+++ Dictionary - Copy/Dictionary/AddWord.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Dictionary
+{
+    public partial class formAddWord : Form
+    {
+        
+        private formDictionary home;
+        public formAddWord(formDictionary home)
+        {
+            InitializeComponent();
+            this.home = home;
+        }
+
+        private void formAddWord_Load(object sender, EventArgs e)
+        {
+            lbAddWordWarning.Text = "";
+            lbAddDefWarning.Text = "";
+        }
+
+        private void btnAddAdd_Click(object sender, EventArgs e)
+        {
+            lbAddWordWarning.Text = "";
+            lbAddDefWarning.Text = "";
+
+            bool valid = true;
+            if (string.IsNullOrWhiteSpace(tbAddWord.Text))
+            {
+                lbAddWordWarning.Text = "Please input word";
+                valid = false;
+            }
+            if (string.IsNullOrWhiteSpace(tbAddDef.Text))
+            {
+                lbAddDefWarning.Text = "Please input definition";
+                valid = false;
+            }
+            if(tbAddWord.Text.Trim().Contains(" "))
+            {
+                lbAddWordWarning.Text = "Word must not contain spaces in between.";
+                valid = false;
+            }
+            if (!valid) return;
+            string date = DateTime.Now.ToString("yyyy-MM-dd");
+
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-I3TO6HF;Initial Catalog=Dictionary;Integrated Security=True");
+            con.Open();
+
+            string query =
+                "INSERT INTO dictionary (word, definition, date) " +
+                "SELECT LOWER(@Word), @Definition, @Date " +
+                "WHERE NOT EXISTS (SELECT 1 FROM dictionary WHERE word = LOWER(@Word)) ";
+
+            //for testing purposes, add word to examples table instead (for generate examples)
+            //string query =
+            //    "INSERT INTO examples (word, definition, date) " +
+            //    "SELECT LOWER(@Word), @Definition, @Date " +
+            //    "WHERE NOT EXISTS (SELECT 1 FROM examples WHERE word = LOWER(@Word)) ";
+
+            SqlCommand cmd = new SqlCommand(query,con);
+            cmd.Parameters.AddWithValue("@Word", tbAddWord.Text.Trim());
+            cmd.Parameters.AddWithValue("@Definition", tbAddDef.Text);
+            cmd.Parameters.AddWithValue("Date", date);
+
+            int rowsAffected = cmd.ExecuteNonQuery();
+            if(rowsAffected == 1)
+            {
+                MessageBox.Show(
+                    "Word added Successfully.",
+                    "Success",
+                    MessageBoxButtons.OK
+                    );
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Word already in the dictionary.",
+                    "Failed",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                    );
+            }
+            con.Close();
+        }
+
+        private void btnAddClear_Click(object sender, EventArgs e)
+        {
+            tbAddDef.Clear();
+            tbAddWord.Clear();
+            lbAddWordWarning.Text = "";
+            lbAddDefWarning.Text = "";
+            lbWordFormat.Show();
+            tbDefFormat.Show();
+        }
+
+        private void llbAddBack_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            home.Show();
+            this.Close();
+        }
+
+        private void formAddWord_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            home.Show();
+        }
+
+        private void tbAddDef_TextChanged(object sender, EventArgs e)
+        {
+            if (tbAddDef.Text == "")
+            {
+                tbDefFormat.Show();
+                return;
+            }
+            else
+                tbDefFormat.Hide();
+
+        }
+
+        private void tbAddWord_TextChanged(object sender, EventArgs e)
+        {
+            if (tbAddWord.Text == "")
+            {
+                lbWordFormat.Show();
+                return;
+            }
+            else
+                lbWordFormat.Hide();
+            
+        }
+
+        private void lbWordFormat_Click(object sender, EventArgs e)
+        {
+            lbWordFormat.Hide();
+            if (string.IsNullOrWhiteSpace(tbAddDef.Text)) tbDefFormat.Show();
+        }
+
+        private void llbExample_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            lbAddWordWarning.Text = "";
+            lbAddDefWarning.Text = "";
+
+            string word, definition;
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-I3TO6HF;Initial Catalog=Dictionary;Integrated Security=True");
+            con.Open();
+
+            string query =
+                "SELECT word, definition " +
+                "FROM examples " +
+                "ORDER BY NEWID()";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataReader rdr = cmd.ExecuteReader();
+            rdr.Read();
+            word = rdr.GetString(0);
+            while(lbWordFormat.Text != "Word" && lbWordFormat.Text == word)
+            {
+                rdr.Read();
+                word = rdr.GetString(0);
+            }
+            word = char.ToUpper(word[0]) + word.Substring(1); //capitalize the word
+            definition = rdr.GetString(1);
+            lbWordFormat.Text = word;
+            tbDefFormat.Text = definition;
+            tbDefFormat.Show();
+            lbWordFormat.Show();
+        }
+
+        private void tbDefFormat_Click(object sender, EventArgs e)
+        {
+            tbDefFormat.Hide();
+            if (string.IsNullOrWhiteSpace(tbAddWord.Text)) lbWordFormat.Show();
+        }
+
+        private void tbAddDef_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbAddWord.Text)) lbWordFormat.Show();
+        }
+
+        private void tbAddWord_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbAddDef.Text)) tbDefFormat.Show();
+        }
+    }
+}
---
+++ Dictionary - Copy/Dictionary/Home.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace Dictionary
+{
+    partial class formDictionary
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.AutoSize = true;
+            this.btnAdd.Location = new System.Drawing.Point(127, 117);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add Word";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(127, 164);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search Word";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(92, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Welcome";
+            // 
+            // formDictionary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnAdd);
+            this.MaximumSize = new System.Drawing.Size(360, 300);
+            this.MinimumSize = new System.Drawing.Size(360, 300);
+            this.Name = "formDictionary";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dictionary";
+            this.Load += new System.EventHandler(this.formDictionary_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
